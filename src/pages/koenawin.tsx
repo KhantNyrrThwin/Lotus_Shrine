ab762fd122141f8bd1918d39a1c109b87c0f3eaf
Ni Ni <nini12121313@gmail.com>
2025-06-22 16:32:37 +0630

    Insert: KoeNaWin Page
    Signed-off-by: Ni Ni <nini12121313@gmail.com>

---
+++ src/pages/koenawin.tsx
@@ -0,0 +1,46 @@
+import "../App.css"
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
+import KoeNaWinGrades from "./KoeNaWinGrades";
+import Buddha from "../assets/KoeNaWinPagoda.png";
+function KoeNaWin(){
+    return(
+        <>
+        <div className=" bg-[#FDE9DA] w-full h-full">
+        <Navbar />
+        <div className="flex flex-col w-full h-[742px] mt-10 font-bold " >
+            <h1 className="text-center mt-14 text-[30px]  text-[#402916]">ကိုးနဝင်း</h1>
+            <h2 className="text-center text-[20px] mt-[14px] mb-[38px] text-[#402916]">“ကိုးနဝင်း မိုးလင်းမှသိမယ်”</h2>
+            <div className="flex flex-row">
+              <img src={Buddha} alt="Description" className="size-[508px]" />
+              <div className="flex flex-col">
+              <div className="  flex items-center justify-center w-[639px] h-[340px] rounded-2xl bg-[#735240]" >                       
+                <ul className="list-disc list-inside  text-white w-[599px] h-[306px]">
+                    <li >ဂုဏ်တော်ကိုးပါးဖြင့်လောကီအကျိုးစီးပွား အောင်မြင်ဖြစ်ထွန်းစေသော အဓိဌာန် ဖြစ်သည်။</li>
+                    <li  >အဓိဋ္ဌာန်(၉)ဆင့်ရှိသည်။</li>
+                    <li>တစ်ဆင့်လျှင်(၉)ရက်နှုန်းဖြင့် ( ၈၁ ) ရက်မပျက်မကွက် ဝင်ရမည်။</li>
+                    <li>ဖော်ပြထားသော ဂုဏ်တော် အမှတ်စဉ် အတိုင်းစိပ်ပါ။</li>
+                    <li> ပုတီးပတ်မှာလည်း ဂုဏ်တော်အမှတ်စဉ် -အတိုင်း ၁ ကျလျှင်တစ်ပတ် စိတ်၍ ၂ ကျလျှင် &nbsp; နှစ်ပတ်စိတ်ရန်ဖြစ်သည်။</li>
+                    <li>  မည်သည့်နေ့တွင်မွေးဖွားသော သားသမီးမဆို တနင်္လာနေ့မှစ၍ အဓိဌာန်ဝင် ရောက်ရမည်။</li>
+                </ul>
+                 </div>
+               <div className="flex flex-row mt-[57px]" >
+              <button className=" flex items-center justify-center w-[260px] h-[73px] rounded-2xl bg-[#4F3016]  text-white mr-[51px] ml-[48px]">ကိုးနဝင်းအဆင့်များ</button>
+              <button className=" flex items-center justify-center w-[260px] h-[73px] rounded-2xl bg-[#4F3016]  text-white">ကိုးနဝင်းဝင်ရောက်မည်</button>
+              </div>
+             
+              
+              </div>
+             
+              
+          </div>
+              
+
+        </div>
+<KoeNaWinGrades/>
+<Footer />
+      </div>  
+</>
+    );
+}
+export default KoeNaWin;
