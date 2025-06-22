cbf734345536bb8deee55f901781cf19344a047f
Ni Ni <nini12121313@gmail.com>
2025-06-22 21:22:02 +0630

    koenawingrades
    Signed-off-by: Ni Ni <nini12121313@gmail.com>

--- src/koe_na_win_grades/eighth.tsx
+++ src/koe_na_win_grades/eighth.tsx
@@ -1,61 +1,66 @@
 import "../App.css";
-function First() {
-  return (  
-  
-    <div className="w-[865px] h-[508px]  bg-[#76403b] p-4 rounded-2xl shadow-md text-orange-950">
+function Eighth() {
+  return (
+       <>      
+    {    
+          <div className="w-[865px] h-[508px]  bg-[#76403b] p-4 rounded-2xl shadow-md text-orange-950">
       <div className=" flex items-center justify-center ">
         <table className=" bg-[#ffffff] rounded-xl overflow- w-[840px] h-[475px] ">
         <tr className=" fy-between px-4 py-2 border-b border-[#D2B48C] ml-[10px]">
        
           <td className="px-13">တနင်္လာ</td>
-          <td className="">သမ္မာသမ္ဗုဒ္ဓေါ </td>
-          <td className="px-2">၂ ပတ် </td>
+          <td className="">ဘဂဝါ</td>
+          <td className="px-2">၉ ပတ် </td>
         </tr>
         <tr className="  fy-between px-4 py-2 border-b  border-[#D2B48C]">
           <td className="px-13">အင်္ဂါ</td>
-          <td>ဘဂဝါ </td>
-          <td className="px-2">၉ ပတ်</td>
+          <td>သတ္တာဒေဝမနုဿာနံ</td>
+          <td className="px-2">၂ ပတ်</td>
         </tr>
         <tr className="fy-between px-4 py-2 border-b  border-[#D2B48C]">
           <td className="px-13">ဗုဒ္ဓဟူး</td>
-          <td>သုဂတော</td>
-          <td className="px-2">၄ ပတ်</td>
+          <td>သမ္မာသမ္ဗုဒ္ဓေါ</td>
+          <td className="px-2">၂ ပတ်</td>
         </tr>
         <tr className="fy-between px-4 py-2 border-b  border-[#D2B48C]">
           <td className="px-13">ကြာသပတေး</td>
-          <td>သတ္တာဒေဝမနုဿာနံ </td>
-          <td className="px-2">၇ ပတ်</td>
-        </tr>
-        <tr className="fy-between px-4 py-2 border-b  border-[#D2B48C]">
-          <td className="px-13">သောကြာ</td>
-          <td>လောကဝိဒူ </td>
+          <td>လောကဝိဒူ</td>
           <td className="px-2">၅ ပတ်</td>
         </tr>
         <tr className="fy-between px-4 py-2 border-b  border-[#D2B48C]">
-          <td className="px-13">စနေ</td>
-          <td>ဝိဇ္ဇာစရဏသမ္ပန္နော </td>
+          <td className="px-13">သောကြာ</td>
+          <td>ဝိဇ္ဇာစရဏသမ္ပန္နော</td>
           <td className="px-2">၃ ပတ်</td>
         </tr>
         <tr className="fy-between px-4 py-2 border-b  border-[#D2B48C]">
-          <td className="px-13">တနင်္ဂနွေ </td>
-          <td>အနုတ္တရောပုရိသ ဓမ္မသာရိထိ</td>
-          <td className="px-2">၆ ပတ် </td>
+          <td className="px-13">စနေ</td>
+          <td>အရဟံ</td>
+          <td className="px-2">၁ ပတ်</td>
+        </tr>
+        <tr className="fy-between px-4 py-2 border-b  border-[#D2B48C]">
+          <td className="px-13">တနင်္ဂနွေ</td>
+          <td>သုဂတော</td>
+          <td className="px-2">၄ ပတ် </td>
         </tr>
         <tr className="fy-between px-4 py-2 border-b  border-[#D2B48C] ">
           <td className="px-13">တနင်္လာ</td>
-          <td>အရဟံ </td>
-          <td className="px-2">၁ ပတ်</td>
+          <td>ဗုဒ္ဓေါ</td>
+          <td className="px-2">၈ ပတ်</td>
         </tr>
         <tr className="fy-between px-4 py-2 ">
           <td className="px-13">အင်္ဂါ</td>
-          <td className="">ဗုဒ္ဓေါ </td>
-          <td className="px-2">၈ ပတ်</td>
+          <td className="">အနုတ္တရောပုရိသ ဓမ္မသာရိထိ</td>
+          <td className="px-2">၆ ပတ်</td>
         </tr>
       </table>
     </div>
     </div>
+     }
+  </> 
+    
+  
   );
   
 }
 
-export default First;
+export default Eighth;
---
+++ src/koe_na_win_grades/third.tsx
@@ -0,0 +1,65 @@
+import "../App.css";
+function Third() {
+  return (
+        <>      
+    {    
+    <div className="w-[865px] h-[508px]  bg-[#76403b] p-4 rounded-2xl shadow-md text-orange-950">
+      <div className=" flex items-center justify-center ">
+        <table className=" bg-[#ffffff] rounded-xl overflow- w-[840px] h-[475px] ">
+        <tr className="fy-between px-4 py-2 border-b  border-[#D2B48C] ml-[10px]">
+          <td>သောကြာ</td>
+          <td>သုဂတော</td>
+          <td> ၄ ပတ် </td>
+        </tr>
+       <tr className="fy-between px-4 py-2 border-b  border-[#D2B48C]">
+          <td>စနေ</td>
+          <td>သမ္မာသမ္ဗုဒ္ဓေါ</td>
+          <td>၂ ပတ်</td>
+        </tr>
+        <tr className="fy-between px-4 py-2 border-b   border-[#D2B48C]">
+          <td>တနင်္ဂနွေ</td>
+          <td>အနုတ္တရောပုရိသ ဓမ္မသာရိထိ</td>
+          <td>၆ ပတ်</td>
+        </tr>
+        <tr className="fy-between px-4 py-2 border-b  border-[#D2B48C]">
+          <td>တနင်္လာ</td>
+          <td>ဘဂဝါ</td>
+          <td>၉ ပတ်</td>
+        </tr>
+        <tr className="fy-between px-4 py-2 border-b  border-[#D2B48C]">
+          <td>အင်္ဂါ</td>
+          <td>သတ္တာဒေဝမနုဿာနံ</td>
+          <td>၇ ပတ်</td>
+        </tr>
+        <tr className="fy-between px-4 py-2 border-b  border-[#D2B48C]">
+          <td>ဗုဒ္ဓဟူး</td>
+          <td>လောကဝိဒူ</td>
+          <td>၅ ပတ်</td>
+        </tr>
+        <tr className="fy-between px-4 py-2 border-  border-[#D2B48C]">
+          <td>ကြာသပတေး</td>
+          <td>ဗုဒ္ဓေါ</td>
+          <td>၈ ပတ်</td>
+        </tr>
+        <tr className="fy-between px-4 py-2 border-b  border-[#D2B48C]">
+          <td>သောကြာ</td>
+          <td>ဝိဇ္ဇာစရဏသမ္ပန္နော</td>
+          <td>၃ ပတ်</td>
+        </tr>
+        <tr className="fy-between px-4 py-2 ">
+          <td>စနေ</td>
+          <td>အရဟံ</td>
+          <td>၁ ပတ်</td>
+        </tr>
+      </table>
+    </div>
+    </div>    
+     }
+  </>
+    
+  
+  );
+  
+}
+
+export default Third;
