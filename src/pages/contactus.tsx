dfee672e06abe28df3b2917809bca80a73c7044c
CodekageNyRR <justicforyang@gmail.com>
2025-07-09 16:40:39 +0630

    Update: TayaTaw
    Signed-off-by: CodekageNyRR <justicforyang@gmail.com>

--- src/pages/contactus.tsx
+++ src/pages/contactus.tsx
@@ -21,7 +21,7 @@ function ContactUs() {
     setLoading(true);
     try {
       const response = await fetch(
-        "http://localhost/lotus_shrine/contact.php",
+        "http://localhost/lotus_shrine/contactUs.php",
         {
           method: "POST",
           headers: {
