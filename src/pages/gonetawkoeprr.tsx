981b3df6b653ff01dfa95382ea56d361b65cf235
Ni Ni <nini12121313@gmail.com>
2025-07-09 11:29:36 +0630

    gonetaw
    Signed-off-by: Ni Ni <nini12121313@gmail.com>

---
+++ src/pages/gonetawkoeprr.tsx
@@ -0,0 +1,49 @@
+import "../App.css";
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
+
+
+
+function Gonetawkoeprr() {
+  return (
+    <>
+            <div className=" bg-[#FDE9DA] w-full h-full">
+        <Navbar />
+         <h1 className="text-center mt-14 text-[30px]  text-[#402916]">
+            ဘုရားဂုဏ်တော် (၉)ပါး
+          </h1>
+       <div className="">
+        
+        <h3 className="ml-150 items-center justify-center w-[350px]  text-center mt-14 text-[20px]  bg-[#4f3016] text-white px-6 py-2 rounded-lg font-semibold shadow ">အရဟံ ၊ အရဟံ ၊ အရဟံ</h3>
+        <p  className=" bg-[#fff] text-center mt-5 text-[20px]  text-[#402916]">ပူဇော်အထူးကို ခံယူတော်မူထိုက်သော မြတ်စွာဘုရား၊ ကိလေသာကင်းစင်တော်မူသော မြတ်စွာဘုရား စိတ်ကွယ်ရာအရပ်၌ပင် မကောင်းမှုကို ပြုတော်မမူသော မြတ်စွာဘုရား။</p>
+
+        <h3 className="ml-160 items-center justify-center w-[260px]  text-center mt-14 text-[20px]  bg-[#4f3016] text-white px-6 py-2 rounded-lg font-semibold shadow ">သမ္မာသမ္ဗုဒ္ဓေါ</h3>
+         <p  className="text-center mt-5 text-[20px]  text-[#402916]">သိမြင်စရာအားလုံးတို့ကို အမှန်အတိုင်း ကိုယ်ပိုင်ဉာဏ်ဖြင့် မခြွင်းမချန် သိမြင်တော်မူသော မြတ်စွာဘုရား။</p>
+
+        <h3 className="ml-160 items-center justify-center w-[260px]  text-center mt-14 text-[20px]  bg-[#4f3016] text-white px-6 py-2 rounded-lg font-semibold shadow ">ဝိဇ္ဇာစရဏ သမ္ပန္နော</h3>
+         <p  className="text-center mt-5 text-[20px]  text-[#402916]">ဝိဇ္ဇာ (၃)ပါး၊ ဝိဇ္ဇာ (၈)ပါး၊ အကျင့်သီလ (၁၅)ပါးတို့နှင့် ပြည့်စုံတော်မူသော မြတ်စွာဘုရား </p>
+
+        <h3 className="ml-160 items-center justify-center w-[260px]  text-center mt-14 text-[20px]  bg-[#4f3016] text-white px-6 py-2 rounded-lg font-semibold shadow ">သုဂတော</h3>
+         <p  className="text-center mt-5 text-[20px]  text-[#402916]"> ကောင်းသောစကား(မှန်ကန်၍အကျိုးရှိသောစကား)ကိုသာဆိုတော်မူသော မြတ်စွာဘုရား။</p>
+
+        <h3 className="ml-160 items-center justify-center w-[260px]  text-center mt-14 text-[20px]  bg-[#4f3016] text-white px-6 py-2 rounded-lg font-semibold shadow ">လောကဝိဒူ</h3>
+         <p  className="text-center mt-5 text-[20px]  text-[#402916]">လောကသုံးပါးကို သိမြင်တော်မူသော မြတ်စွာဘုရား။ ( သတ္တလောက ၊ ဩကာသလောက ၊ သင်္ခါရလောက) </p>
+
+        <h3 className="ml-155 items-center justify-center w-[300px]  text-center mt-14 text-[20px]  bg-[#4f3016] text-white px-6 py-2 rounded-lg font-semibold shadow ">အနုတ္တရောပုရိသဓမ္မသာရထိ</h3>
+         <p  className="text-center mt-5 text-[20px]  text-[#402916]">ဆုံးမထိုက်သော သတ္တဝါတို့ကို အတုမရှိ ဆုံးမတော်မူသော မြတ်စွာဘုရား။</p>
+
+        <h3 className="ml-160 items-center justify-center w-[260px]  text-center mt-14 text-[20px]  bg-[#4f3016] text-white px-6 py-2 rounded-lg font-semibold shadow ">သတ္တာဒေဝမနုဿနံ</h3>
+         <p  className="text-center mt-5 text-[20px]  text-[#402916]">နတ်၊ လူတို့၏ ဆရာတစ်ဆူ ဖြစ်တော်မူသော မြတ်စွာဘုရား။ </p>
+
+        <h3 className="ml-160 items-center justify-center w-[260px]  text-center mt-14 text-[20px]  bg-[#4f3016] text-white px-6 py-2 rounded-lg font-semibold shadow ">ဗုဒ္ဓေါ</h3>
+         <p  className="text-center mt-5 text-[20px]  text-[#402916]">သစ္စာလေးပါးကို ကိုယ်တိုင်လည်းသိတော်မူ၊ သူတပါးတို့ကိုလည်း သိအောင်ဟောတော်မူသော မြတ်စွာဘုရား</p>
+         
+        <h3 className="ml-160 items-center justify-center w-[260px]  text-center mt-14 text-[20px]  bg-[#4f3016] text-white px-6 py-2 rounded-lg font-semibold shadow ">ဘဂဝါ</h3>
+         <p  className="text-center mt-5 mb-10 text-[20px]  text-[#402916]">ဘုန်းတော်ခြောက်ပါး နှင့် ပြည့်စုံတော်မူသော မြတ်စွာဘုရား၊ (ဣဿရိယဘုန်းတော်၊ ဓမ္မဘုန်းတော်၊ ယသဘုန်းတော်၊ သီရိဘုန်းတော်၊ ကာမဘုန်းတော်၊ ပယတ္တဘုန်းတော်) </p>
+       </div>
+        <Footer />
+      </div>
+    </>
+  );
+}
+export default Gonetawkoeprr;
