75652103b1c2bcf8a37c568bdfa1eebb215f1b16
CodekageNyRR <justicforyang@gmail.com>
2025-08-14 15:29:21 +0630

    feat: Update UI
    Signed-off-by: CodekageNyRR <justicforyang@gmail.com>

--- src/pages/meditation.tsx
+++ src/pages/meditation.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
-import { Play, Pause, SkipBack, SkipForward, Volume2, VolumeX } from "lucide-react";
+import { Play } from "lucide-react";
 import bell from "../assets/sounds/Meditaion.mp3";
 import meditationAudio from "../assets/sounds/Meditaion.mp3";
 import { useMusicPlayer } from "../components/MusicPlayerContext";
