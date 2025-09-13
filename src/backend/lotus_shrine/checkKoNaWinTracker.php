55090f1aff0fe69b86852d327ce0ab1990b94c11
Raven <paiminthway13@gmail.com>
2025-09-13 14:15:44 +0630

    Mediation
    Signed-off-by: Raven <paiminthway13@gmail.com>

--- src/backend/lotus_shrine/checkKoNaWinTracker.php
+++ src/backend/lotus_shrine/checkKoNaWinTracker.php
@@ -1,7 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: http://localhost:5173");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization"); // Add Authorization if you use tokens
+header("Access-Control-Allow-Headers: Content-Type, Authorization"); 
 header("Access-Control-Allow-Credentials: true");
 header("Content-Type: application/json");
 
